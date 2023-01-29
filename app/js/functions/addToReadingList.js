254dee9f5c90fcf061e0bd6e177116553211cdaa
StevenLignereux <steven.lignereux@hotmail.fr>
2023-01-29 06:44:52 +0100

    add session storage code and display it from session infos

--- app/js/functions/addToReadingList.js
+++ app/js/functions/addToReadingList.js
@@ -1,36 +1,39 @@
 export function addToReadingList(event) {
     // Récupération des données du livre
     let bookData = event.target.parentElement.dataset;
-    console.log(bookData)
+    let book = JSON.stringify(bookData);
 
     // Stockage en session
-    if(sessionStorage) {
-        // Stocker les données dans le sessionStorage
-        sessionStorage.setItem(bookData.title, JSON.stringify(bookData));
-    } else {
-        console.log("SessionStorage n'est pas supporté par votre navigateur");
-    }
+    let books = sessionStorage.getItem("books") ? JSON.parse(sessionStorage.getItem("books")) : [];
+    books.push(book);
+    sessionStorage.setItem("books", JSON.stringify(books));
 
-    // Création de l'élément HTML représentant le livre dans la poch'liste
-    let bookItem = document.createElement('div');
-    bookItem.classList.add('reading-list__book');
+    // Récupération des livres stockés en session
+    let booksList = JSON.parse(sessionStorage.getItem('books'));
 
-    bookItem.innerHTML = `
+    // Boucle pour afficher tous les livres stockés
+    for (let i = 0; i < booksList.length; i++) {
+        let book = JSON.parse(booksList[i]);
+
+        // Création de l'élément HTML représentant le livre dans la poch'liste
+        let bookItem = document.createElement('div');
+        bookItem.classList.add('reading-list__book');
+
+        bookItem.innerHTML = `
         <div class="result-div__book card">
             <div class="heading">
-                <h3 class="card__title">${bookData.bookTitle}</h3>
-                <i class="fa-regular fa-bookmark card__bookmark"></i>
+                <h3 class="card__title">${book.bookTitle}</h3>
+                <i class="fa-solid fa-trash card__bookmark"></i>
             </div>
-            <h3 class="card__title" >${bookData.bookId}</h3>
-            <p class="card__author" >${bookData.bookAuthor}</p>
-            <p class="card__description">${bookData.bookDescription}</p>
-            <img class="card__image" data-book-thumbnail="${bookData.bookThumbnail}" src="${bookData.bookThumbnail}" alt="${bookData.bookThumbnail}">
+            <h3 class="card__title" >${book.bookId}</h3>
+            <p class="card__author" >${book.bookAuthor}</p>
+            <p class="card__description">${book.bookDescription}</p>
+            <img class="card__image" data-book-thumbnail="${book.bookThumbnail}" src="${book.bookThumbnail}" alt="${book.bookThumbnail}">
         </div>`;
 
-    // Ajout de l'élément à la poch'liste
-    let readingList = document.getElementById('content');
-    readingList.appendChild(bookItem);
-
-
+        // Ajout de l'élément à la poch'liste
+        let readingList = document.getElementById('content');
+        readingList.appendChild(bookItem);
+    }
 }
 
