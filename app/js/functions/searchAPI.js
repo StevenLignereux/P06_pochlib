853ce327754ab5a2a04af8f808dc77df000e4044
StevenLignereux <steven.lignereux@hotmail.fr>
2023-01-31 11:19:32 +0100

    stylish cards for a grid layout

--- app/js/functions/searchAPI.js
+++ app/js/functions/searchAPI.js
@@ -39,16 +39,18 @@ export function searchAPI() {
                         thumbnail = "../../../assets/images/unavailable.png";
                     }
                     resultDiv.innerHTML += `
-                    <div class="result-div__book card">
-                        <div class="heading"  data-book-id="${id}" data-book-title="${title}"  data-book-author="${author}" data-book-description="${description}" data-book-thumbnail="${thumbnail}" >
-                            <h3 class="card__title">${title}</h3>
-                            <i class="fa-solid fa-bookmark card__bookmark"></i>
+                        <div class="cards">
+                            <div class="result-div__book card ">
+                                <div class="heading"  data-book-id="${id}" data-book-title="${title}"  data-book-author="${author}" data-book-description="${description}" data-book-thumbnail="${thumbnail}" >
+                                    <h3 class="card__title">${title}</h3>
+                                    <i class="fa-solid fa-bookmark card__bookmark"></i>
+                                </div>
+                                <h3 class="card__title" >${id}</h3>
+                                <p class="card__author" >${author}</p>
+                                <p class="card__description">${description}</p>
+                                <img class="card__image" data-book-thumbnail="${thumbnail}" src="${thumbnail}" alt="${thumbnail}">
+                            </div>
                         </div>
-                        <h3 class="card__title" >${id}</h3>
-                        <p class="card__author" >${author}</p>
-                        <p class="card__description">${description}</p>
-                        <img class="card__image" data-book-thumbnail="${thumbnail}" src="${thumbnail}" alt="${thumbnail}">
-                    </div>
                 `;
 
 
