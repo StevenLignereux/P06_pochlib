b97810aa14d6317fe81ff8797a19a772a86d95ee
StevenLignereux <steven.lignereux@hotmail.fr>
2023-02-03 11:45:46 +0100

    refacto displayBooks.js

--- app/js/functions/displayBooks.js
+++ app/js/functions/displayBooks.js
@@ -1,8 +1,9 @@
 import {removeFromReadingList} from "./removeFromReadingList.js";
 
-export function displayBooks() {
+export const displayBooks = () => {
     // We take books in session
     let booksList = JSON.parse(sessionStorage.getItem("books")) || [];
+
 
     // Can be added inside function ( clearContentBookmark )
     let readingList = document.getElementById("content");
@@ -12,10 +13,10 @@ export function displayBooks() {
 
     let bookItem = document.createElement("div");
     bookItem.setAttribute("id", "card-container")
-    
+
     // Display books store in session
-    for (let i = 0; i < booksList.length; i++) {
-        let book = JSON.parse(booksList[i]);
+    for (const book of booksList) {
+        let parsedBook = JSON.parse(book);
 
         // Html element create for displaying books
 
@@ -23,13 +24,13 @@ export function displayBooks() {
              <div class="cards">
                 <div class="result-div__book card">
                     <div class="heading">
-                        <h3 class="card__title">${book.bookTitle}</h3>
+                        <h3 class="card__title">${parsedBook.bookTitle}</h3>
                          <i class="fa-solid fa-trash card__bookmark" id="trash"></i>
                     </div>
-                    <h3 class="card__title">${book.bookId}</h3>
-                    <p class="card__author">${book.bookAuthor}</p>
-                    <p class="card__description">${book.bookDescription}</p>
-                    <img class="card__image" data-book-thumbnail="${book.bookThumbnail}" loading="lazy" src="${book.bookThumbnail}" alt="${book.bookThumbnail}">
+                    <h3 class="card__title">${parsedBook.bookId}</h3>
+                    <p class="card__author">${parsedBook.bookAuthor}</p>
+                    <p class="card__description">${parsedBook.bookDescription}</p>
+                    <img class="card__image" data-book-thumbnail="${parsedBook.bookThumbnail}" loading="lazy" src="${parsedBook.bookThumbnail}" alt="${parsedBook.bookThumbnail}">
                 </div>
             </div>
     `;
@@ -38,6 +39,6 @@ export function displayBooks() {
 
     // Add element to the poch list
     readingList.appendChild(bookItem);
-    document.addEventListener('click', removeFromReadingList);
+    readingList.addEventListener('click', removeFromReadingList);
 
 }
